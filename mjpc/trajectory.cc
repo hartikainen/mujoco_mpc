c6706b4c16f522c225dbf475fbae6e5acd6f4952
taylor <taylorathanielhowell@gmail.com>
2022-12-13 16:30:01 -0800

    transition in rollout

--- mjpc/trajectory.cc
+++ mjpc/trajectory.cc
@@ -108,6 +108,9 @@ void Trajectory::Rollout(
   times[0] = time;
   data->time = time;
 
+  // transition
+  task->Transition(model, data);
+
   // step1
   mj_step1(model, data);
 
@@ -138,6 +141,9 @@ void Trajectory::Rollout(
     mju_copy(DataAt(states, t * dim_state + model->nq + model->nv), data->act,
              model->na);
     times[t] = data->time;
+
+    // transition
+    task->Transition(model, data);
 
     // step1
     mj_step1(model, data);
@@ -180,6 +186,9 @@ void Trajectory::Rollout(
     mju_zero(DataAt(actions, (horizon - 1) * dim_action), dim_action);
   }
 
+  // transition
+  task->Transition(model, data);
+
   // final step1
   mj_step1(model, data);
 
@@ -221,6 +230,9 @@ void Trajectory::RolloutDiscrete(
   times[0] = time;
   data->time = time;
 
+  // transition
+  task->Transition(model, data);
+
   // step1
   mj_step1(model, data);
 
@@ -252,6 +264,9 @@ void Trajectory::RolloutDiscrete(
     mju_copy(DataAt(states, t * dim_state + model->nq + model->nv), data->act,
              model->na);
     times[t] = data->time;
+
+    // transition
+    task->Transition(model, data);
 
     // step1
     mj_step1(model, data);
@@ -293,6 +308,9 @@ void Trajectory::RolloutDiscrete(
   } else {
     mju_zero(DataAt(actions, (horizon - 1) * dim_action), dim_action);
   }
+
+  // transition
+  task->Transition(model, data);
 
   // final step1
   mj_step1(model, data);

f1fe3c774a3c71c5edf2c6956e6187b6e55c5be8
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2022-12-16 13:53:59 -0800

    Rename `KeyFrameByName` -> `KeyQPosByName`

--- mjpc/main.cc
+++ mjpc/main.cc
@@ -816,7 +816,7 @@ void PhysicsLoop(mj::Simulate& sim) {
       }
 
       // set initial configuration via keyframe
-      double* qpos_key = mjpc::KeyFrameByName(sim.mnew, sim.dnew, "home");
+      double* qpos_key = mjpc::KeyQPosByName(sim.mnew, sim.dnew, "home");
       if (qpos_key) {
         mju_copy(sim.dnew->qpos, qpos_key, sim.mnew->nq);
       }
@@ -1071,7 +1071,7 @@ int main(int argc, char** argv) {
   printf("Agent threads: %i\n", sim->agent.max_threads());
 
   // set initial configuration via keyframe
-  double* qpos_key = mjpc::KeyFrameByName(sim->mnew, sim->dnew, "home");
+  double* qpos_key = mjpc::KeyQPosByName(sim->mnew, sim->dnew, "home");
   if (qpos_key) {
     mju_copy(sim->dnew->qpos, qpos_key, sim->mnew->nq);
   }

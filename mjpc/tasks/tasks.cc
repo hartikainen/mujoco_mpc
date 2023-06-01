01a4a65d0db3cc14013fd5581d0fb8cc29879504
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2023-06-01 20:20:31 +0100

    Add humanoid skateboard to tasks

--- mjpc/tasks/tasks.cc
+++ mjpc/tasks/tasks.cc
@@ -20,6 +20,7 @@
 #include "mjpc/tasks/acrobot/acrobot.h"
 #include "mjpc/tasks/cartpole/cartpole.h"
 #include "mjpc/tasks/hand/hand.h"
+#include "mjpc/tasks/humanoid/skateboard/skateboard.h"
 #include "mjpc/tasks/humanoid/stand/stand.h"
 #include "mjpc/tasks/humanoid/tracking/tracking.h"
 #include "mjpc/tasks/humanoid/walk/walk.h"
@@ -39,6 +40,7 @@ std::vector<std::shared_ptr<Task>> GetTasks() {
     std::make_shared<Acrobot>(),
     std::make_shared<Cartpole>(),
     std::make_shared<Hand>(),
+    std::make_shared<humanoid::Skateboard>(),
     std::make_shared<humanoid::Stand>(),
     std::make_shared<humanoid::Tracking>(),
     std::make_shared<humanoid::Walk>(),

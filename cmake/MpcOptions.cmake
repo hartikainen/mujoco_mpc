026c5dd7c00a0f66551f9d74836fb1c215816592
Elias Mikkola <elias@singa.com>
2024-02-04 15:35:34 +0200

    ignore compiler flags

--- cmake/MpcOptions.cmake
+++ cmake/MpcOptions.cmake
@@ -87,9 +87,9 @@ get_mujoco_extra_link_options(EXTRA_LINK_OPTIONS)
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT MSVC))
   set(EXTRA_COMPILE_OPTIONS
       -Werror
-      -Wall
+      # -Wall
       -Wimplicit-fallthrough
-      -Wunused
+      # -Wunused
       -Wvla
       -Wno-int-in-bool-context
       -Wno-sign-compare
